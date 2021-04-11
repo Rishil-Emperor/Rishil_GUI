e444b12b93b8e81f8165f257103e7b9acfe763fa
Rishil Kadakia <rishil.kadakia@gmail.com>
2021-04-11 12:11:38 -0700

    Opened time_zones.json into time.py. Added variables for current year, current month, and current date.

--- functions/time.py
+++ functions/time.py
@@ -1,8 +1,29 @@
-from datetime import datetime
+import datetime
 import pytz
 import json
 
+with open('time_zones.json', 'r') as f:
+    time_zones = json.load(f)
 
+# This is where the GUI (PyQt5) code will be.
 
-
-
+today_date = datetime.date.today()
+today_year = today_date.year
+month = today_date.month
+today_day = today_date.day
+months = {
+    "1": "January",
+    "2": "February",
+    "3": "March",
+    "4": "April",
+    "5": "May",
+    "6": "June",
+    "7": "July",
+    "8": "August",
+    "9": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December"
+}
+today_month = months[str(month)]
+print([today_year, today_month, today_day])
