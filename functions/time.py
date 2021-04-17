84c71717845ad8db8dc2e4a911e1159c8aefcf1a
Rishil Kadakia <rishil.kadakia@gmail.com>
2021-04-17 13:19:02 -0700

    Added christmas as a datetime variable and added untilChristmas which shows days until Christmas.

--- functions/time.py
+++ functions/time.py
@@ -52,6 +52,18 @@ for x in str(days_until_birthday):                                              
     if x == ',':
         break
     else:
-        untilBirthday += x                                                                                              # untilBirthday is updated to show only number of days left.
+        untilBirthday += x                                                                                              # untilBirthday variable is updated to show only number of days left.
 
-print(birthday, untilBirthday)
+if month == 12 and today_day > 25:                                                                                      # If Christmas has already passed this year...
+    christmas_year = today_year + 1                                                                                     # christmas_year will be set for next year.
+else:                                                                                                                   # If Christmas has not passed this year...
+    christmas_year = today_year                                                                                         # christmas_year will be set for this year.
+
+christmas = datetime.date(christmas_year, 12, 25)                                                                       # Set Christmas as December 25th, christmas_year as a variable called christmas.
+days_until_christmas = christmas - today_date                                                                           # days_until_christmas are how many days are left from the current day until Christmas.
+untilChristmas = ''                                                                                                     # Creates a new variable called untilChristmas which is set as an empty string for now.
+for x in str(days_until_christmas):                                                                                     # Iterating through days_until_christmas to get it to only show number of days and nothing else.
+    if x == ',':
+        break
+    else:
+        untilChristmas += x                                                                                             # untilChristmas variable is updated to show only number of days left.
